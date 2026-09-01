8e196a55de5ae9a49647264915a7d585a6445ba1
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-01 18:22:54 +0530

    Bitwise X--or Operator

---
+++ Java/Operator/BitwiseXorOperator.java
@@ -0,0 +1,11 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate bitwise XOR operator (^)
+*/
+public class BitwiseXorOperator {
+    public static void main(String[] args) {
+        int a = 6, b = 4;
+        System.out.println("Bitwise XOR => " + (a ^ b));
+    }
+}
