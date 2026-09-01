80f40343b1db37372fad8cc98c1e6427df593080
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-01 18:24:51 +0530

    Chained - Assignment

---
+++ Java/Operator/ChainedAssignment.java
@@ -0,0 +1,12 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate chained assignment (=)
+*/
+public class ChainedAssignment {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = b = c = 100;
+        System.out.println("a => " + a + ", b => " + b + ", c => " + c);
+    }
+}
