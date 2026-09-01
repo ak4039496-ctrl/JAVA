f145c3be33b46675d2b11da2caca039a3abd9a13
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-01 18:11:59 +0530

    BitwiseAnd Operator

---
+++ Java/Operator/BitwiseAndOperator.java
@@ -0,0 +1,11 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate bitwise AND operator (&)
+*/
+public class BitwiseAndOperator {
+    public static void main(String[] args) {
+        int a = 6, b = 4; // 6=110, 4=100
+        System.out.println("Bitwise AND => " + (a & b));
+    }
+}
