9d7d39bf1d4fd13ddb827eafbb37a26fade0dbce
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-01 18:16:05 +0530

    Bitwise-Not-Operator

---
+++ Java/Operator/BitwiseNotOperator.java
@@ -0,0 +1,11 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate bitwise NOT operator (~)
+*/
+public class BitwiseNotOperator {
+    public static void main(String[] args) {
+        int a = 5; // binary 0101
+        System.out.println("Bitwise NOT => " + ~a);
+    }
+}
