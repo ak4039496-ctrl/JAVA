093bad1544bf3e1ab90c92698fdca8ea49f5cf21
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-01 18:15:29 +0530

    Bitwise-And-Operator

---
+++ Java/Operator/BitwiseCombination.java
@@ -0,0 +1,13 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate combination of bitwise operators
+*/
+public class BitwiseCombination {
+    public static void main(String[] args) {
+        int a = 6, b = 4;
+        System.out.println("a & b => " + (a & b));
+        System.out.println("a | b => " + (a | b));
+        System.out.println("a ^ b => " + (a ^ b));
+    }
+}
