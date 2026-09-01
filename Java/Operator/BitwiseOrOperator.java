5a5a8a7701422095b0d8335481dd57d08710a3bd
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-01 18:18:33 +0530

    Bitwise  - Not Operator

---
+++ Java/Operator/BitwiseOrOperator.java
@@ -0,0 +1,11 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate bitwise OR operator (|)
+*/
+public class BitwiseOrOperator {
+    public static void main(String[] args) {
+        int a = 6, b = 4;
+        System.out.println("Bitwise OR => " + (a | b));
+    }
+}
