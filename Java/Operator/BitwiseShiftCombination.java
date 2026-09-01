8a1adc62a9251d98280f8a9d2bbdf1b64dd1e17c
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-01 18:19:22 +0530

    Bitwise  - Shift-Combination

---
+++ Java/Operator/BitwiseShiftCombination.java
@@ -0,0 +1,12 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate combined left and right shift operators
+*/
+public class BitwiseShiftCombination {
+    public static void main(String[] args) {
+        int a = 8; // binary 1000
+        System.out.println("Left Shift (a<<2) = " + (a << 2));
+        System.out.println("Right Shift (a>>1) = " + (a >> 1));
+    }
+}
