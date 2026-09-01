a0e606a2ddeb3a24c8712626085177fd68477b58
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-01 18:11:34 +0530

    Assignment Operator

---
+++ Java/Operator/AssignmentOperator.java
@@ -0,0 +1,12 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate assignment operator (=)
+*/
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int a;
+        a = 100; // assignment
+        System.out.println("Value of a => " + a);
+    }
+}
