1261e67c2aa502e9347234cfba8d27ce6ea3c116
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-01 18:07:50 +0530

    ArrayIndexOperator

---
+++ Java/Operator/ArrayIndexOperator.java
@@ -0,0 +1,12 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate array indexing operator []
+*/
+public class ArrayIndexOperator {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30};
+        System.out.println("First element => " + arr[0]);
+        System.out.println("Second element => " + arr[1]);
+    }
+}
