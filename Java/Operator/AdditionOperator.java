f3cfaba0aeb912c44d97d6d5d1ad698f23747682
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-01 17:58:14 +0530

    java code first upload

---
+++ Java/Operator/AdditionOperator.java
@@ -0,0 +1,11 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate addition operator (+)
+*/
+public class AdditionOperator {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println("Sum => " + (a + b));
+    }
+}
