d86387f62a436d8ba9a5189441f7650a14577689
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-02 22:14:17 +0530

    Compound--Subtraction

---
+++ Java/Operator/CompoundSubtraction.java
@@ -0,0 +1,12 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate compound assignment operator (-=)
+*/
+public class CompoundSubtraction {
+    public static void main(String[] args) {
+        int a = 20;
+        a -= 7;
+        System.out.println("Value of a => " + a);
+    }
+}
