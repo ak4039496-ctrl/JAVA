3a8e55460821da2912b0c9628cc5e4f7fb69d7c9
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-02 22:12:58 +0530

    Compound--Chaining

---
+++ Java/Operator/CompoundChaining.java
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate chained compound assignment
+*/
+public class CompoundChaining {
+    public static void main(String[] args) {
+        int a = 10;
+        a += 5;
+        a *= 2;
+        a -= 4;
+        System.out.println("Final value of a => " + a);
+    }
+}
