ad229c552d72e8fc56449cdbde3c02070326158a
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-02 22:12:32 +0530

    Compound-Addition

---
+++ Java/Operator/CompoundAddition.java
@@ -0,0 +1,12 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate compound assignment operator (+=)
+*/
+public class CompoundAddition {
+    public static void main(String[] args) {
+        int a = 10;
+        a += 5;
+        System.out.println("Value of a => " + a);
+    }
+}
