801904e12173241dc8ad72e9ea8809d25e937ae8
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-02 22:13:26 +0530

    Compound--Division

---
+++ Java/Operator/CompoundDivision.java
@@ -0,0 +1,12 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate compound assignment operator (/=)
+*/
+public class CompoundDivision {
+    public static void main(String[] args) {
+        int a = 20;
+        a /= 4;
+        System.out.println("Value of a => " + a);
+    }
+}
