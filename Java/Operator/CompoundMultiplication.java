4238714508251f1f3a90cfc6756cb98e2e525d20
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-02 22:13:53 +0530

    Compound-Multiplication

---
+++ Java/Operator/CompoundMultiplication.java
@@ -0,0 +1,12 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate compound assignment operator (*=)
+*/
+public class CompoundMultiplication {
+    public static void main(String[] args) {
+        int a = 4;
+        a *= 3;
+        System.out.println("Value of a => " + a);
+    }
+}
