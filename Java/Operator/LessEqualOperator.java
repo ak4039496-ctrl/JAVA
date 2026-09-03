a3d8a7c28ba202d90f21d00796d2515e72b6d1af
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-03 09:37:10 +0530

    Less equal operator

---
+++ Java/Operator/LessEqualOperator.java
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate relational operator (<=)
+*/
+public class LessEqualOperator {
+    public static void main(String[] args) {
+        int a = 15, b = 20;
+        if (a <= b)
+            System.out.println("a is less or equal to b");
+        else
+            System.out.println("a is greater than b");
+    }
+}
