0e3713463186e5bb917f5a8d66303d0746509783
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-03 09:38:33 +0530

    Logical and Operator

---
+++ Java/Operator/LogicalAndOperator.java
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate logical AND operator (&&)
+*/
+public class LogicalAndOperator {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        if (a > 0 && b > 0)
+            System.out.println("Both numbers are positive");
+        else
+            System.out.println("Condition not satisfied");
+    }
+}
