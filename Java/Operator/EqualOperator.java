b60a4aa425c10b72098443f28e35ce2f858173f5
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-03 09:32:18 +0530

    equal Operator

---
+++ Java/Operator/EqualOperator.java
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate relational operator (==)
+*/
+public class EqualOperator {
+    public static void main(String[] args) {
+        int a = 10, b = 10;
+        if (a == b)
+            System.out.println("a and b are equal");
+        else
+            System.out.println("a and b are not equal");
+    }
+}
