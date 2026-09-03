4995a928aa22c840588b5fc1faf1821e50b05328
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-03 09:36:38 +0530

    left shift operator

---
+++ Java/Operator/LeftShiftOperator.java
@@ -0,0 +1,11 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate left shift operator (<<)
+*/
+public class LeftShiftOperator {
+    public static void main(String[] args) {
+        int a = 5; // binary 0101
+        System.out.println("Left Shift (a<<1) => " + (a << 1));
+    }
+}
