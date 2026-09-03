91fff104d2d34c5c21a6f3ebbaa90738e07bba2e
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-03 09:35:10 +0530

    increment operator

---
+++ Java/Operator/IncrementOperator.java
@@ -0,0 +1,13 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate increment operator (++)
+*/
+public class IncrementOperator {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("Original a => " + a);
+        a++;
+        System.out.println("After increment a => " + a);
+    }
+}
