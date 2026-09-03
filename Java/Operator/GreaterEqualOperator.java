7830be25cb5c8dd7171751d373f6db1f1dd4eff9
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-03 09:33:04 +0530

    Greater equal operator

---
+++ Java/Operator/GreaterEqualOperator.java
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate relational operator (>=)
+*/
+public class GreaterEqualOperator {
+    public static void main(String[] args) {
+        int a = 20, b = 20;
+        if (a >= b)
+            System.out.println("a is greater or equal to b");
+        else
+            System.out.println("a is less than b");
+    }
+}
