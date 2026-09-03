4f8ce6d96df0fba84a013550fb5170b6187c6ac2
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-03 09:33:33 +0530

    Greater operator

---
+++ Java/Operator/GreaterOperator.java
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate relational operator (>)
+*/
+public class GreaterOperator {
+    public static void main(String[] args) {
+        int a = 25, b = 20;
+        if (a > b)
+            System.out.println("a is greater than b");
+        else
+            System.out.println("a is not greater than b");
+    }
+}
