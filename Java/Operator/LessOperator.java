9706fa5dec46230093d7eecf79e9d4631e753827
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-03 09:37:42 +0530

    Less  operator

---
+++ Java/Operator/LessOperator.java
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate relational operator (<)
+*/
+public class LessOperator {
+    public static void main(String[] args) {
+        int a = 15, b = 30;
+        if (a < b)
+            System.out.println("a is less than b");
+        else
+            System.out.println("a is not less than b");
+    }
+}
