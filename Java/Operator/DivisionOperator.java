37535444fb5b067b1b64fe36965a399508151f42
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-03 09:31:41 +0530

    division - Operator

---
+++ Java/Operator/DivisionOperator.java
@@ -0,0 +1,11 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate division operator (/)
+*/
+public class DivisionOperator {
+    public static void main(String[] args) {
+        int a = 40, b = 5;
+        System.out.println("Quotient => " + (a / b));
+    }
+}
