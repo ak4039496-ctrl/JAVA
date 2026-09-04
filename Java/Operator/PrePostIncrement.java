f086dba520a11768f82582bc7b8f5fa12da87248
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-04 20:24:07 +0530

    Pre - Post -Increment

---
+++ Java/Operator/PrePostIncrement.java
@@ -0,0 +1,13 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate pre and post increment
+*/
+public class PrePostIncrement {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("Pre-increment => " + (++a));
+        System.out.println("Post-increment => " + (a++));
+        System.out.println("Final value of a => " + a);
+    }
+}
