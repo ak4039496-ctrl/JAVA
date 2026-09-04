045c85e1584b249ebfed20bfac7391d632ea52df
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-04 20:18:41 +0530

    Modulus Operator

---
+++ Java/Operator/ModulusOperator.java
@@ -0,0 +1,11 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate modulus operator (%)
+*/
+public class ModulusOperator {
+    public static void main(String[] args) {
+        int a = 17, b = 5;
+        System.out.println("Remainder => " + (a % b));
+    }
+}
