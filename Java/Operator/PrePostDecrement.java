b7e2085e6b76532416e6375dec1043963cf1b432
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-04 20:23:01 +0530

    Pre post decrement

---
+++ Java/Operator/PrePostDecrement.java
@@ -0,0 +1,13 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate pre and post decrement
+*/
+public class PrePostDecrement {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("Pre-decrement => " + (--a));
+        System.out.println("Post-decrement => " + (a--));
+        System.out.println("Final value of a => " + a);
+    }
+}
