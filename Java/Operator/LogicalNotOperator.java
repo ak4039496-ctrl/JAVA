0992728705d710d354108fa55e8196ab4cad14bb
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-04 20:17:26 +0530

    Logical not Operator

---
+++ Java/Operator/LogicalNotOperator.java
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate logical NOT operator (!)
+*/
+public class LogicalNotOperator {
+    public static void main(String[] args) {
+        boolean flag = false;
+        if (!flag)
+            System.out.println("Flag is false");
+        else
+            System.out.println("Flag is true");
+    }
+}
