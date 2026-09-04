3692cde2ba027b3f80eaaabc9dbb9e80449d49d4
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-04 20:21:39 +0530

    Notequal operator

---
+++ Java/Operator/NotEqualOperator.java
@@ -0,0 +1,14 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate relational operator (!=)
+*/
+public class NotEqualOperator {
+    public static void main(String[] args) {
+        int a = 10, b = 15;
+        if (a != b)
+            System.out.println("a and b are not equal");
+        else
+            System.out.println("a and b are equal");
+    }
+}
