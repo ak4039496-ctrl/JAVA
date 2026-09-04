30c517982d73ce1c0a5b0e8a9fdbec9e25ef4a2f
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-04 20:19:40 +0530

    Multiplication-- Operator

---
+++ Java/Operator/MultiplicationOperator.java
@@ -0,0 +1,11 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate multiplication operator (*)
+*/
+public class MultiplicationOperator {
+    public static void main(String[] args) {
+        int a = 7, b = 6;
+        System.out.println("Product => " + (a * b));
+    }
+}
