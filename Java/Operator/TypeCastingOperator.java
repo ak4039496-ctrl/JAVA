bba9132b1ec8af6891eebbcfa71cab295cf88725
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-05 15:58:42 +0530

    Type- casting operator

---
+++ Java/Operator/TypeCastingOperator.java
@@ -0,0 +1,12 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate type casting operator
+*/
+public class TypeCastingOperator {
+    public static void main(String[] args) {
+        int a = 7, b = 2;
+        double result = (double) a / b; // explicit cast
+        System.out.println("Result => " + result);
+    }
+}
