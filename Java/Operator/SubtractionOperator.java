91c819e7693f49ce5bb251dcede110f48af91131
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-05 15:56:59 +0530

    Subtraction operator

---
+++ Java/Operator/SubtractionOperator.java
@@ -0,0 +1,11 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate subtraction operator (-)
+*/
+public class SubtractionOperator {
+    public static void main(String[] args) {
+        int a = 50, b = 30;
+        System.out.println("Difference => " + (a - b));
+    }
+}
