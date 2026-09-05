45d418d221ffdb5acfb811fdc3daf2d61cc902f6
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-05 15:57:39 +0530

    Ternary  -operator

---
+++ Java/Operator/TernaryOperator.java
@@ -0,0 +1,12 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate ternary operator (? :)
+*/
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int max = (a > b) ? a : b;
+        System.out.println("Maximum => " + max);
+    }
+}
