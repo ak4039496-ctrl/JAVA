a195c1a7d873b29ce5822b4642fa4c47e352c72a
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-05 15:54:32 +0530

    Shift -left -right

---
+++ Java/Operator/ShiftLeftRight.java
@@ -0,0 +1,12 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate left and right shift operators
+*/
+public class ShiftLeftRight {
+    public static void main(String[] args) {
+        int a = 16; // binary 10000
+        System.out.println("Left Shift (a<<2) => " + (a << 2));
+        System.out.println("Right Shift (a>>2) => " + (a >> 2));
+    }
+}
