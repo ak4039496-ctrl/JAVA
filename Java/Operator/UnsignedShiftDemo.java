d238ab9d25981f7d83989b57baeb7b700dfe4b32
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-05 16:01:02 +0530

    Unsigned shift -demo

---
+++ Java/Operator/UnsignedShiftDemo.java
@@ -0,0 +1,11 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate unsigned right shift operator (>>>)
+*/
+public class UnsignedShiftDemo {
+    public static void main(String[] args) {
+        int a = -8;
+        System.out.println("Unsigned Right Shift (a>>>1) => " + (a >>> 1));
+    }
+}
