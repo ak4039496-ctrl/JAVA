e366959e5faa6679ce4138f6e156b15757e66043
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-05 15:56:32 +0530

    String  -concatenation operator

---
+++ Java/Operator/StringConcatenationOperator.java
@@ -0,0 +1,12 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate string concatenation operator (+)
+*/
+public class StringConcatenationOperator {
+    public static void main(String[] args) {
+        String first = "Hello";
+        String second = "World";
+        System.out.println("Concatenated String => " + (first + " " + second));
+    }
+}
