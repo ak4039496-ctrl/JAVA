5b5037e1a0c670aac90e31673563ea094e48f29e
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-05 16:00:26 +0530

    Unsigned -right shift operator

---
+++ Java/Operator/UnsignedRightShiftOperator.java
@@ -0,0 +1,11 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate unsigned right shift operator (>>>)
+*/
+public class UnsignedRightShiftOperator {
+    public static void main(String[] args) {
+        int a = -20;
+        System.out.println("Unsigned Right Shift (a>>>2) => " + (a >>> 2));
+    }
+}
