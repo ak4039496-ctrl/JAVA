a3c47ae33997f6a4da7014d2baac039d08de229d
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-05 15:53:59 +0530

    Right shift operator

---
+++ Java/Operator/RightShiftOperator.java
@@ -0,0 +1,11 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate right shift operator (>>)
+*/
+public class RightShiftOperator {
+    public static void main(String[] args) {
+        int a = 20; // binary 10100
+        System.out.println("Right Shift (a>>2) => " + (a >> 2));
+    }
+}
