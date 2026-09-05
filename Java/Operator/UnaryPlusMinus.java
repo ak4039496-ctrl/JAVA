97027f841bba23a5faf68494d5eec0347f59f709
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-05 15:59:47 +0530

    Unary -plus minus

---
+++ Java/Operator/UnaryPlusMinus.java
@@ -0,0 +1,12 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate unary plus and minus operators
+*/
+public class UnaryPlusMinus {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("Unary Plus => " + (+a));
+        System.out.println("Unary Minus => " + (-a));
+    }
+}
