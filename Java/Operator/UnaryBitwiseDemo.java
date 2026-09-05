8a19955a7394e5b77b4e7482a65388ff56de6a72
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-05 15:59:13 +0530

    Unary bitwise  -demo

---
+++ Java/Operator/UnaryBitwiseDemo.java
@@ -0,0 +1,11 @@
+/*
+Author: Amit Gupta
+Date: 08-08-2026
+Program: Demonstrate unary bitwise NOT (~)
+*/
+public class UnaryBitwiseDemo {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("Bitwise NOT of a => " + ~a);
+    }
+}
