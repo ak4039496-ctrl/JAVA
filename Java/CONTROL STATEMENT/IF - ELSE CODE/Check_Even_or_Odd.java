185686fe6069e6d2f836ea89778b672fee4de5a2
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-09 16:33:50 +0530

    Check -Even or -Odd

---
+++ Java/CONTROL STATEMENT/IF - ELSE CODE/Check_Even_or_Odd.java
@@ -0,0 +1,18 @@
+/*
+Author: Amit Gupta
+Date: 18-Aug-2026
+Program: Check if a number is Even or Odd 
+*/
+
+public class Check_Even_or_Odd {
+    public static void main(String[] args) {
+        int num = 7; // Example number
+
+        // if-else to check even or odd
+        if(num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
