ce869be82d8110591450e5a0c5e285667553763f
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-10 19:01:08 +0530

    Check -Largest of -Two

---
+++ Java/CONTROL STATEMENT/Check_Largest_Of_Two.java
@@ -0,0 +1,18 @@
+/*
+Author: Amit Gupta
+Date: 20-Aug-2026
+Program: Find the largest of two numbers
+*/
+
+public class Check_Largest_Of_Two {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        // if-else to find largest
+        if(a > b) {
+            System.out.println("A is Largest");
+        } else {
+            System.out.println("B is Largest");
+        }
+    }
+}
