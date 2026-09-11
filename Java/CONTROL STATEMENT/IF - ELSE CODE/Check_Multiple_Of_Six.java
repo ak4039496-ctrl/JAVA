0676c082799c1adb66f1716f311b0ae7eab281b8
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-11 09:46:13 +0530

    Check -multiple Of Six

---
+++ Java/CONTROL STATEMENT/IF - ELSE CODE/Check_Multiple_Of_Six.java
@@ -0,0 +1,24 @@
+/*
+Author: Amit Gupta
+Date: 23-Aug-2026
+Program: Check if a number is Multiple of 6
+*/
+
+import java.util.Scanner;
+
+public class Check_Multiple_Of_Six {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number:- ");
+        int num = sc.nextInt();
+
+        if(num % 6 == 0) {
+            System.out.println(num + " is a Multiple of 6");
+        } else {
+            System.out.println(num + " is Not a Multiple of 6");
+        }
+
+        sc.close(); 
+    }
+}
