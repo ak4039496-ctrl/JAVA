b60fa44c7abd8121fecd0bdfb42990d5932bb875
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-12 09:47:37 +0530

    Check -Number -Type

---
+++ Java/CONTROL STATEMENT/IF - ELSE CODE/Check_Number_Type.java
@@ -0,0 +1,26 @@
+/*
+Author: Amit Gupta
+Date: 27-Aug-2026
+Program: Check if number is Positive, Negative or Zero
+*/
+
+import java.util.Scanner;
+
+public class Check_Number_Type {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number:- ");
+        int num = sc.nextInt();
+
+        if(num > 0) {
+            System.out.println("Positive Number");
+        } else if(num < 0) {
+            System.out.println("Negative Number");
+        } else {
+            System.out.println("Number is Zero");
+        }
+
+        sc.close();
+    }
+}
