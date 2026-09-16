e627ebe0219c9652c256e7094f1e3e4cf9438a91
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-16 11:45:37 +0530

    color -meaning

---
+++ Java/CONTROL STATEMENT/SWITCH CASE/color_meaning.java
@@ -0,0 +1,18 @@
+// Author: Amit Gupta
+// Date: 16-09-2026
+// Program: Color meaning
+
+public class color_meaning {
+    public static void main(String[] args) {
+        
+        String color = "Blue";
+        
+        switch(color) {
+            case "Red": System.out.println("Represents Energy"); break;
+            case "Green": System.out.println("Represents Nature"); break;
+            case "Blue": System.out.println("Represents Calmness"); break;
+            case "Yellow": System.out.println("Represents Happiness"); break;
+            default: System.out.println("Unknown color!");
+        }
+    }
+}
