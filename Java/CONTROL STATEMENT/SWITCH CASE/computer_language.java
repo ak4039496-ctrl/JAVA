965bd35cedfa8898b0d06be0ee245b4173bc9f4c
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-16 11:46:37 +0530

    computer -language

---
+++ Java/CONTROL STATEMENT/SWITCH CASE/computer_language.java
@@ -0,0 +1,18 @@
+// Author: Amit Gupta
+// Date: 16-09-2026
+// Program: Computer language
+
+public class computer_language {
+    public static void main(String[] args) {
+        
+        int choice = 3;
+        
+        switch(choice) {
+            case 1: System.out.println("C"); break;
+            case 2: System.out.println("C++"); break;
+            case 3: System.out.println("Java"); break;
+            case 4: System.out.println("Python"); break;
+            default: System.out.println("Invalid choice!");
+        }
+    }
+}
