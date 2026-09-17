d32c799d1e8df365e17a2b367bb29db15cfdf27c
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-17 09:49:19 +0530

    day Number to -name

---
+++ Java/CONTROL STATEMENT/SWITCH CASE/day_number_to_name.java
@@ -0,0 +1,20 @@
+// Author: Amit Gupta
+// Date: 16-09-2026
+// Program: Convert day number to name
+public class day_number_to_name {
+    public static void main(String[] args) {
+        
+        int day = 5; 
+        
+        switch(day) {
+            case 1: System.out.println("Monday"); break;
+            case 2: System.out.println("Tuesday"); break;
+            case 3: System.out.println("Wednesday"); break;
+            case 4: System.out.println("Thursday"); break;
+            case 5: System.out.println("Friday"); break;
+            case 6: System.out.println("Saturday"); break;
+            case 7: System.out.println("Sunday"); break;
+            default: System.out.println("Invalid day!");
+        }
+    }
+}
