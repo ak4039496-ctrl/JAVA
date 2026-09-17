931f6b209190b6f015112bad5201b4f84a73d70c
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-17 09:45:23 +0530

    computer Parts

---
+++ Java/CONTROL STATEMENT/SWITCH CASE/computer_parts.java
@@ -0,0 +1,18 @@
+// Author: Amit Gupta
+// Date: 16-09-2026
+// Program: Computer parts
+
+public class computer_parts {
+    public static void main(String[] args) {
+        
+        int choice = 3;
+        
+        switch(choice) {
+            case 1: System.out.println("CPU"); break;
+            case 2: System.out.println("Monitor"); break;
+            case 3: System.out.println("Keyboard"); break;
+            case 4: System.out.println("Mouse"); break;
+            default: System.out.println("Invalid choice!");
+        }
+    }
+}
