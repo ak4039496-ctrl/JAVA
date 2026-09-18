1ff5fbcd2b3f1005a04abcee7703459b10a408b0
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-18 10:08:21 +0530

    fruit -menu

---
+++ Java/CONTROL STATEMENT/SWITCH CASE/fruit_menu.java
@@ -0,0 +1,18 @@
+// Author: Amit Gupta
+// Date: 17-09-2026
+// Program: Fruit menu
+
+public class fruit_menu {
+    public static void main(String[] args) {
+        
+        int choice = 2;
+        
+        switch(choice) {
+            case 1: System.out.println("Apple"); break;
+            case 2: System.out.println("Banana"); break;
+            case 3: System.out.println("Mango"); break;
+            case 4: System.out.println("Orange"); break;
+            default: System.out.println("Invalid choice!");
+        }
+    }
+}
