2cc568e16bd5df0a03d96868aa000c1bbe6914bb
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-19 06:19:19 +0530

    language -menu

---
+++ Java/CONTROL STATEMENT/SWITCH CASE/language_menu.java
@@ -0,0 +1,18 @@
+// Author: Amit Gupta
+// Date: 16-09-2026
+// Program: Language menu
+
+public class language_menu {
+    public static void main(String[] args) {
+        
+        int choice = 2;
+        
+        switch(choice) {
+            case 1: System.out.println("English"); break;
+            case 2: System.out.println("Hindi"); break;
+            case 3: System.out.println("French"); break;
+            case 4: System.out.println("Spanish"); break;
+            default: System.out.println("Invalid choice!");
+        }
+    }
+}
