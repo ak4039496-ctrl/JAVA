27abcbbf2bc22c630237ba44e0ef32c519d5b214
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-19 06:18:01 +0530

    holiday Type

---
+++ Java/CONTROL STATEMENT/SWITCH CASE/holiday_type.java
@@ -0,0 +1,17 @@
+// Author: Amit Gupta
+// Date: 16-09-2026
+// Program: Holiday type
+
+public class holiday_type {
+    public static void main(String[] args) {
+        
+        int choice = 1;
+        
+        switch(choice) {
+            case 1: System.out.println("National Holiday"); break;
+            case 2: System.out.println("Festival Holiday"); break;
+            case 3: System.out.println("Weekend Holiday"); break;
+            default: System.out.println("Invalid holiday!");
+        }
+    }
+}
