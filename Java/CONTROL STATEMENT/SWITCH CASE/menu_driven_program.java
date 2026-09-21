134e8c0c47304ee5d099326c1ec56fbb5b17411e
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-21 19:51:50 +0530

    menu -driven program

---
+++ Java/CONTROL STATEMENT/SWITCH CASE/menu_driven_program.java
@@ -0,0 +1,17 @@
+// Author: Amit Gupta
+// Date: 18-09-2026
+// Program: Menu-driven program 
+
+public class menu_driven_program {
+    public static void main(String[] args) {
+        
+        int choice = 2; 
+        
+        switch(choice) {
+            case 1: System.out.println("Hello"); break;
+            case 2: System.out.println("Welcome"); break;
+            case 3: System.out.println("Goodbye"); break;
+            default: System.out.println("Invalid choice!");
+        }
+    }
+}
