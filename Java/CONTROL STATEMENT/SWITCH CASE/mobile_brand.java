fd29b79a653d30f897d289d976703f261e6ac709
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-21 19:52:29 +0530

    mobile Brand

---
+++ Java/CONTROL STATEMENT/SWITCH CASE/mobile_brand.java
@@ -0,0 +1,18 @@
+// Author: Amit Gupta
+// Date: 18-09-2026
+// Program: Mobile brand
+
+public class mobile_brand {
+    public static void main(String[] args) {
+        
+        int choice = 1;
+        
+        switch(choice) {
+            case 1: System.out.println("Samsung"); break;
+            case 2: System.out.println("Apple"); break;
+            case 3: System.out.println("Xiaomi"); break;
+            case 4: System.out.println("OnePlus"); break;
+            default: System.out.println("Invalid choice!");
+        }
+    }
+}
