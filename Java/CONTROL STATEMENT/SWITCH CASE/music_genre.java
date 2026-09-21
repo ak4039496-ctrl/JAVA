bbda45a2aec701f9785d1ac9896a73deada264a3
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-21 19:56:39 +0530

    music -genre

---
+++ Java/CONTROL STATEMENT/SWITCH CASE/music_genre.java
@@ -0,0 +1,18 @@
+// Author: Amit Gupta
+// Date: 19-09-2026
+// Program: Music genre
+
+public class music_genre {
+    public static void main(String[] args) {
+        
+        int choice = 4;
+        
+        switch(choice) {
+            case 1: System.out.println("Classical"); break;
+            case 2: System.out.println("Rock"); break;
+            case 3: System.out.println("Pop"); break;
+            case 4: System.out.println("Jazz"); break;
+            default: System.out.println("Invalid genre!");
+        }
+    }
+}
