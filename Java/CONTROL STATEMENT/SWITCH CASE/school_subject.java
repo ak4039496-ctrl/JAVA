fde65d9026f4dd5becdf7df64450210a489a932f
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-22 22:36:46 +0530

    school -subject

---
+++ Java/CONTROL STATEMENT/SWITCH CASE/school_subject.java
@@ -0,0 +1,18 @@
+// Author: Amit Gupta
+// Date: 19-09-2026
+// Program: School subject
+
+public class school_subject {
+    public static void main(String[] args) {
+        
+        int choice = 2;
+        
+        switch(choice) {
+            case 1: System.out.println("Math"); break;
+            case 2: System.out.println("Science"); break;
+            case 3: System.out.println("English"); break;
+            case 4: System.out.println("History"); break;
+            default: System.out.println("Invalid subject!");
+        }
+    }
+}
