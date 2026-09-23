17cd3b6f477310b607141aab2c8c9ad67d8d8394
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-23 12:17:35 +0530

    sports -menu

---
+++ Java/CONTROL STATEMENT/SWITCH CASE/sports_menu.java
@@ -0,0 +1,18 @@
+// Author: Amit Gupta
+// Date: 19-09-2026
+// Program: Sports menu
+
+public class sports_menu {
+    public static void main(String[] args) {
+        
+        int choice = 4;
+        
+        switch(choice) {
+            case 1: System.out.println("Cricket"); break;
+            case 2: System.out.println("Football"); break;
+            case 3: System.out.println("Hockey"); break;
+            case 4: System.out.println("Badminton"); break;
+            default: System.out.println("Invalid choice!");
+        }
+    }
+}
