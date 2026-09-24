05480a0c3e284dfc5e3a2ed1bf6a9b8d46d3f952
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-24 06:43:38 +0530

    transport - mode

---
+++ Java/CONTROL STATEMENT/SWITCH CASE/transport_mode.java
@@ -0,0 +1,18 @@
+// Author: Amit Gupta
+// Date: 20-09-2026
+// Program: Transport mode
+
+public class transport_mode {
+    public static void main(String[] args) {
+        
+        int choice = 2;
+        
+        switch(choice) {
+            case 1: System.out.println("Car"); break;
+            case 2: System.out.println("Bus"); break;
+            case 3: System.out.println("Train"); break;
+            case 4: System.out.println("Airplane"); break;
+            default: System.out.println("Invalid choice!");
+        }
+    }
+}
