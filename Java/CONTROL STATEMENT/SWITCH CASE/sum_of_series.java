33a83da2fa204d6cdfe1fcd34b0131ade149360f
AMIT--GUPTA --HLO <ak4039496@gmail.com>
2026-09-24 06:41:51 +0530

    sum of -series

---
+++ Java/CONTROL STATEMENT/SWITCH CASE/sum_of_series.java
@@ -0,0 +1,28 @@
+// Author: Amit Gupta
+// Date: 20-09-2026
+// Program: Sum of first n natural numbers
+
+public class sum_of_series {
+    public static void main(String[] args) {
+        
+        int n = 10; // terms
+        int choice = 1;
+        
+        switch(choice) {
+            
+            case 1:
+                int sum = 0;
+                
+                // Loop to add numbers
+                for(int i = 1; i <= n; i++) {
+                    sum += i;
+                }
+                
+                System.out.println("Sum of first " + n + " numbers => " + sum);
+                break;
+                
+            default:
+                System.out.println("Invalid choice!");
+        }
+    }
+}
